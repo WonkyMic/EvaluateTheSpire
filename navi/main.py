a665efbfe0d108a1c2ebce69288218b01a1a6bc5
gaggle331 <hafezg@email.arizona.edu>
2018-09-05 21:11:24 -0700

    Created neural network and added logic to receive json in API call and respond with output of network.

--- navi/main.py
+++ navi/main.py
@@ -1,13 +1,36 @@
-from flask import Flask 
+from flask import Flask
 from flask import jsonify
 from flask import request
 
-app = Flask(__name__)
+from dqn import DQNAgent
 
-@app.route('/navi/test-endpoint', methods=['GET', 'POST'])
-def navi():
-	content = request.get_json()
-	return jsonify(content)
+app = Flask(__name__)
+agent = DQNAgent()
+
+@app.route('/navi/StateDataDumptest-endpoint', methods=['GET', 'POST'])
+def state():
+    content = request.get_json()
+    agent.stateData = content
+    return True
+    #return jsonify(content)
+
+@app.route('/navi/CombatDataDumptest-endpoint', methods=['GET', 'POST'])
+def combat():
+    content = request.get_json()
+    current_state, combat_state = agent.create_combined_dataframe(agent.stateData, content)
+
+    data = current_state.values
+    combat_action = agent.predict_combat_action(data)
+    mydict = {
+        'x1': [x.tolist() for x in combat_action]
+    }
+
+    return jsonify(mydict)
+
+@app.route('/navi/EventDataDumptest-endpoint', methods=['GET', 'POST'])
+def event():
+    content = request.get_json()
+    #return jsonify(content)
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
